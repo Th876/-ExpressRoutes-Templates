12e1a180380abeee3898281b91ab3f15fd16e907
TH876 <tayhiggins14@gmail.com>
2023-05-09 21:20:33 -0400

    Final Edit

--- server.js
+++ server.js
@@ -1,12 +1,3 @@
-// Index : Show all the things!
-// New : An empty form for a new thing
-// Delete : Get rid of this particular thing!
-// Update : Update this specific thing with this updated form
-// Create : Make a new thing with this filled out form
-// Edit : A prefilled form to update a specific thing
-// Show : Show me this one thing!
-
-
 const express = require('express');
 
 const mysteryonecasts = require('./models/mysteryonecasts.js');
